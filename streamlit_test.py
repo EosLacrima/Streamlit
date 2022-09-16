988ba7b2243ad204810274b0c158ebe3ea2b3d95
EosLacrima <58544705+EosLacrima@users.noreply.github.com>
2022-09-16 12:14:03 +0800

    Update streamlit_test.py

--- streamlit_test.py
+++ streamlit_test.py
@@ -1,10 +1,5 @@
 import streamlit as st
 import pandas as pd
 
-st.write("""
-# My first app
-Hello *world!*
-""")
+st.write(r"# My first app" + \n + "Hello *world!*")
 
-df = pd.read_csv("./点云3月.csv")
-st.line_chart(df)
